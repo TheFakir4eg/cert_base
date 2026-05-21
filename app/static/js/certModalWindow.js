37a9d916989c1de8c9007755d0917a3a885cc920
Панков Михаил Александрович <miha_pankoff@mail.ru>
2026-05-21 12:35:32 +0300

    исправления от 21/05/26

--- app/static/js/certModalWindow.js
+++ app/static/js/certModalWindow.js
@@ -5,11 +5,16 @@ const editingBtn = document.getElementById("editCertBtn");
 const spendBtn = document.getElementById("spendCertBtn");
 const closingBtn = document.getElementById("closingCertBtn");
 const restoreBtn = document.getElementById("restoreCertBtn");
+const searchToggleBtn = document.getElementById("searchToggleBtn");
+const searchBox = document.getElementById("searchBox");
+const searchInput = document.getElementById("certSearchInput");
+const clearSearchBtn = document.getElementById("clearSearchBtn");
 
 const spendModal = new bootstrap.Modal(document.getElementById("spendCertModal"));
 
 let selectedRow = null;
 let selectedCertId = null;
+let currentSearch = "";
 
 const tbody = document.getElementById("certTableBody");
 
@@ -30,6 +35,63 @@ tbody.addEventListener("dblclick", async (e) => {
     await openUsageHistory(certId);
 });
 
+//работа кнопки "Поиск" (лупа)
+if (searchToggleBtn && searchBox && searchInput) {
+
+    searchToggleBtn.addEventListener("click", () => {
+
+        searchToggleBtn.classList.add("d-none");
+
+        searchBox.classList.remove("d-none");
+
+        requestAnimationFrame(() => {
+            searchBox.classList.add("active");
+        });
+
+        searchInput.focus();
+    });
+}
+
+if (clearSearchBtn) {
+
+    clearSearchBtn.addEventListener("click", () => {
+
+        currentSearch = "";
+
+        searchInput.value = "";
+
+        applyFilter(getCurrentFilter());
+
+        searchBox.classList.remove("active");
+
+        setTimeout(() => {
+            searchBox.classList.add("d-none");
+            searchToggleBtn.classList.remove("d-none");
+        }, 250);
+    });
+}
+
+if (searchInput) {
+
+    searchInput.addEventListener("input", () => {
+
+        currentSearch = searchInput.value
+            .trim()
+            .toLowerCase();
+
+        applyFilter(getCurrentFilter());
+    });
+}
+
+function getCurrentFilter() {
+
+    const activeBtn = document.querySelector(".filter-btn.active");
+
+    return activeBtn
+        ? activeBtn.dataset.filter
+        : "all";
+}
+
 function selectRow(row) {
 
     document.querySelectorAll(".cert-row")
@@ -44,13 +106,28 @@ function selectRow(row) {
     const balance = parseFloat(row.dataset.balance);
     const hasClient = row.dataset.clientId && row.dataset.clientId !== "";
 
-    editingBtn.disabled = false;
-    issuingBtn.disabled = hasClient || !active;
-    spendBtn.disabled = !hasClient || !active || balance <= 0;
-    closingBtn.disabled = !active || balance > 0;
-    restoreBtn.disabled = active;
+    if (editingBtn) {
+        editingBtn.disabled = false;
+    }
+
+    if (issuingBtn) {
+        issuingBtn.disabled = hasClient || !active;
+    }
+
+    if (spendBtn) {
+        spendBtn.disabled = !hasClient || !active || balance <= 0;
+    }
+
+    if (closingBtn) {
+        closingBtn.disabled = !active || balance > 0;
+    }
+
+    if (restoreBtn) {
+        restoreBtn.disabled = active;
+    }
 }
 
+// функция показа истории транзакций
 async function openUsageHistory(certId) {
 
     const response = await fetch(`/certificates/${certId}/usages`);
@@ -83,91 +160,29 @@ async function openUsageHistory(certId) {
     ).show();
 }
 
-document.getElementById("editCertBtn").addEventListener("click", () => {
+// работа кнопки "Изменить"
+if (editingBtn) {
+    document.getElementById("editCertBtn").addEventListener("click", () => {
 
-    if (!selectedRow) return;
+        if (!selectedRow) return;
 
-    const modalElement = document.getElementById("editCertModal");
-    const modal = new bootstrap.Modal(modalElement);
+        const modalElement = document.getElementById("editCertModal");
+        const modal = new bootstrap.Modal(modalElement);
 
-    // ⭐ ищем поля ТОЛЬКО внутри edit modal
-    const form = modalElement.querySelector("form");
+        // ⭐ ищем поля ТОЛЬКО внутри edit modal
+        const form = modalElement.querySelector("form");
 
-    form.querySelector("[name=cert_id]").value = selectedRow.dataset.id;
-    form.querySelector("[name=reason]").value = selectedRow.dataset.reason;
-    form.querySelector("[name=series]").value = selectedRow.dataset.series;
-    form.querySelector("[name=number]").value = selectedRow.dataset.number;
-    form.querySelector("[name=total_amount]").value = selectedRow.dataset.totalAmount;
-    form.querySelector("[name=servicegroup_id]").value = selectedRow.dataset.servicegroupId;
-    form.querySelector("[name=note]").value = selectedRow.dataset.note;
+        form.querySelector("[name=cert_id]").value = selectedRow.dataset.id;
+        form.querySelector("[name=reason]").value = selectedRow.dataset.reason;
+        form.querySelector("[name=series]").value = selectedRow.dataset.series;
+        form.querySelector("[name=number]").value = selectedRow.dataset.number;
+        form.querySelector("[name=total_amount]").value = selectedRow.dataset.totalAmount;
+        form.querySelector("[name=servicegroup_id]").value = selectedRow.dataset.servicegroupId;
+        form.querySelector("[name=note]").value = selectedRow.dataset.note;
 
-    modal.show();
-});
-
-// document.querySelectorAll(".filter-btn").forEach(btn => {
-//     btn.addEventListener("click", () => {
-
-//         document.querySelectorAll(".filter-btn")
-//             .forEach(b => b.classList.remove("active"));
-
-//         btn.classList.add("active");
-
-//         const filter = btn.dataset.filter;
-
-//         document.querySelectorAll(".cert-row").forEach(row => {
-
-//             const status = row.dataset.status;
-
-//             if (filter === "all" || status === filter) {
-//                 row.style.display = "";
-//             } else {
-//                 row.style.display = "none";
-//             }
-//         });
-//     });
-// });
-
-// document.querySelectorAll(".filter-btn").forEach(btn => {
-//     btn.addEventListener("click", () => {
-
-//         document.querySelectorAll(".filter-btn")
-//             .forEach(b => b.classList.remove("active"));
-
-//         btn.classList.add("active");
-
-//         const filter = btn.dataset.filter;
-
-//         document.querySelectorAll(".cert-row").forEach(row => {
-
-//             const status = row.dataset.status;
-//             const active = row.dataset.active;
-
-//             // ⭐ вкладка "Выведены"
-//             if (filter === "out") {
-//                 if (active === "false" || active === "null") {
-//                     row.style.display = "";
-//                 } else {
-//                     row.style.display = "none";
-//                 }
-//                 return;
-//             }
-
-//             // ⭐ остальные вкладки показывают только активные
-//             if (active !== "true") {
-//                 row.style.display = "none";
-//                 return;
-//             }
-
-//             // ⭐ обычная фильтрация по статусу
-//             if (filter === "all" || status === filter) {
-//                 row.style.display = "";
-//             } else {
-//                 row.style.display = "none";
-//             }
-
-//         });
-//     });
-// });
+        modal.show();
+    });
+}
 
 document.querySelectorAll(".filter-btn").forEach(btn => {
     btn.addEventListener("click", () => {
@@ -188,49 +203,103 @@ function applyFilter(filter) {
         const status = row.dataset.status;
         const active = row.dataset.active;
 
-        // вкладка "Выведены"
+        // ===== фильтр вкладок =====
+
+        let visible = true;
+
         if (filter === "out") {
-            if (active === "false" || active === "null") {
-                row.style.display = "";
-            } else {
-                row.style.display = "none";
-            }
-            return;
-        }
 
-        // остальные вкладки показывают только активные
-        if (active !== "true") {
-            row.style.display = "none";
-            return;
-        }
+            visible = (
+                active === "false" ||
+                active === "null"
+            );
 
-        // обычная фильтрация по статусу
-        if (filter === "all" || status === filter) {
-            row.style.display = "";
         } else {
-            row.style.display = "none";
+
+            if (active !== "true") {
+                visible = false;
+            }
+
+            if (
+                visible &&
+                filter !== "all" &&
+                status !== filter
+            ) {
+                visible = false;
+            }
         }
+
+        // ===== поиск =====
+
+        if (visible && currentSearch) {
+
+            const rowText = row.innerText.toLowerCase();
+
+            visible = rowText.includes(currentSearch);
+        }
+
+        row.style.display = visible ? "" : "none";
     });
 }
 
+// function applyFilter(filter) {
+
+//     document.querySelectorAll(".cert-row").forEach(row => {
+
+//         const status = row.dataset.status;
+//         const active = row.dataset.active;
+
+//         // вкладка "Выведены"
+//         if (filter === "out") {
+//             if (active === "false" || active === "null") {
+//                 row.style.display = "";
+//             } else {
+//                 row.style.display = "none";
+//             }
+//             return;
+//         }
+
+//         // остальные вкладки показывают только активные
+//         if (active !== "true") {
+//             row.style.display = "none";
+//             return;
+//         }
+
+//         // обычная фильтрация по статусу
+//         if (filter === "all" || status === filter) {
+//             row.style.display = "";
+//         } else {
+//             row.style.display = "none";
+//         }
+//     });
+// }
+
 function updateButtonsState() {
-    editingBtn.disabled = !selectedRow;
-    issuingBtn.disabled = !selectedRow;
+    if (editingBtn) {
+        editingBtn.disabled = !selectedRow;
+    }
+
+    if (issuingBtn) {
+        issuingBtn.disabled = !selectedRow;
+    }
 }
 
 const issueModal = new bootstrap.Modal(document.getElementById('issueCertModal'));
 
-issuingBtn.addEventListener("click", () => {
-    if (!selectedRow) return;
+// работа кнопки "Выдача"
+if (issuingBtn) {
+    issuingBtn.addEventListener("click", () => {
+        if (!selectedRow) return;
 
-    document.getElementById("issue_cert_id").value = selectedRow.dataset.id;
+        document.getElementById("issue_cert_id").value = selectedRow.dataset.id;
 
-    // сегодня
-    const today = new Date().toISOString().split('T')[0];
-    document.getElementById("issue_date").value = today;
+        // дат выдачи - сегодня
+        const today = new Date().toISOString().split('T')[0];
+        document.getElementById("issue_date").value = today;
 
-    issueModal.show();
-});
+        issueModal.show();
+    });
+}
 
 document.getElementById("issueCertForm").addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -251,69 +320,86 @@ document.getElementById("issueCertForm").addEventListener("submit", async (e) =>
     if (response.ok) location.reload();
 });
 
-spendBtn.addEventListener("click", () => {
-    if (!selectedRow) return;
+// работа кнопки "Списание"
+if (spendBtn) {
+    spendBtn.addEventListener("click", () => {
+        if (!selectedRow) return;
 
-    document.getElementById("spend_cert_id").value = selectedRow.dataset.id;
-    document.getElementById("spend_balance").value = selectedRow.dataset.balance;
+        document.getElementById("spend_cert_id").value = selectedRow.dataset.id;
+        document.getElementById("spend_balance").value = selectedRow.dataset.balance;
 
-    document.getElementById("spend_amount").value = "";
-    document.getElementById("spend_comment").value = "";
+        document.getElementById("spend_amount").value = "";
+        document.getElementById("spend_comment").value = "";
 
-    spendModal.show();
-});
-
-
-
-closingBtn.addEventListener("click", async () => {
-    if (!selectedRow) return;
-
-    const certId = selectedRow.dataset.id;
-
-    const response = await fetch(`/certificates/${certId}/close`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" }
+        spendModal.show();
     });
+}
 
-    const result = await response.json();
+// работа кнопки "Вывод"
+if (closingBtn) {
+    closingBtn.addEventListener("click", async () => {
+        if (!selectedRow) return;
 
-    if (response.ok) {
-        showFlash("Сертификат успешно выведен", "success");
-        setTimeout(() => location.reload(), 1500);
-    } else {
-        alert(result.error || "Ошибка");
-    }
-});
+        const certId = selectedRow.dataset.id;
 
-restoreBtn.addEventListener("click", async () => {
-    if (!selectedRow) return;
+        const response = await fetch(`/certificates/${certId}/close`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" }
+        });
 
-    const certId = selectedRow.dataset.id;
+        const result = await response.json();
 
-    const response = await fetch(`/certificates/${certId}/restore`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" }
+        if (response.ok) {
+            showFlash("Сертификат успешно выведен", "success");
+            setTimeout(() => location.reload(), 1500);
+        } else {
+            alert(result.error || "Ошибка");
+        }
     });
+}
 
-    const result = await response.json();
+// работа кнопки "Восстановить"
+if (restoreBtn) {
+    restoreBtn.addEventListener("click", async () => {
+        if (!selectedRow) return;
 
-    if (response.ok) {
-        showFlash("Сертификат восстановлен", "success");
-        setTimeout(() => location.reload(), 1500);
-    } else {
-        alert(result.error || "Ошибка");
-    }
-});
+        const certId = selectedRow.dataset.id;
+
+        const response = await fetch(`/certificates/${certId}/restore`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" }
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            showFlash("Сертификат восстановлен", "success");
+            setTimeout(() => location.reload(), 1500);
+        } else {
+            alert(result.error || "Ошибка");
+        }
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    //активируем фильтр при старте страницы
-    const activeTab = document.querySelector(".filter-btn.active");
+
+    // вкладка по умолчанию
+    let activeTab = document.querySelector(".filter-btn.active");
+
+    // если активной нет — берем первую доступную
+    if (!activeTab) {
+        activeTab = document.querySelector(".filter-btn");
+    }
+
+    // применяем фильтр
     if (activeTab) {
+        activeTab.classList.add("active");
         applyFilter(activeTab.dataset.filter);
     }
 
     const spendForm = document.getElementById("spendCertForm");
-    if (!spendForm) return;   // защита от падения
+
+    if (!spendForm) return;
 
     spendForm.addEventListener("submit", async (e) => {
         e.preventDefault();
@@ -339,5 +425,4 @@ document.addEventListener("DOMContentLoaded", () => {
             alert(result.message);
         }
     });
-
 });
